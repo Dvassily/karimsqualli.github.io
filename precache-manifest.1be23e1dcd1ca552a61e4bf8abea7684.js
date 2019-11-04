77b202f853644246a6637b81330f43dc8134f5a9
karimsqualli <karim.squalli@usmba.ac.ma>
2019-11-04 15:24:54 +0100

    inital

---
+++ precache-manifest.1be23e1dcd1ca552a61e4bf8abea7684.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5d83704e08b6290a04",
+    "url": "/css/chunk-vendors.48edf0fd.css"
+  },
+  {
+    "revision": "ccbe982f4eeb1fbf29cf73f49b2a5cc4",
+    "url": "/img/0.jpeg"
+  },
+  {
+    "revision": "8417422099aeefa7a7ec8d43278d7fb0",
+    "url": "/img/1.jpeg"
+  },
+  {
+    "revision": "e8496a4e04ce96982910e3f1698895d5",
+    "url": "/img/2.jpeg"
+  },
+  {
+    "revision": "e67eec4979609fbe7aab8b979792cdaa",
+    "url": "/img/3.jpeg"
+  },
+  {
+    "revision": "41332f4feac22b4106edda556f864202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dac8e79d6b513ff56d5f",
+    "url": "/js/app.683a4cdf.js"
+  },
+  {
+    "revision": "0f5d83704e08b6290a04",
+    "url": "/js/chunk-vendors.e4342620.js"
+  },
+  {
+    "revision": "16709316661bd6667dbf7e8428767b97",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
