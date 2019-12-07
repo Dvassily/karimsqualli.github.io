fe16968372d3fa2036bbbcc54d4fff2e0f7d6878
Squalli karim <rusted1012@gmail.com>
2019-12-07 16:43:55 +0100

    new ver :

---
+++ precache-manifest.0bd48204189ff68cd5df0742a333d718.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5d83704e08b6290a04",
+    "url": "/css/chunk-vendors.48edf0fd.css"
+  },
+  {
+    "revision": "ccbe982f4eeb1fbf29cf73f49b2a5cc4",
+    "url": "/img/0.jpeg"
+  },
+  {
+    "revision": "8417422099aeefa7a7ec8d43278d7fb0",
+    "url": "/img/1.jpeg"
+  },
+  {
+    "revision": "e8496a4e04ce96982910e3f1698895d5",
+    "url": "/img/2.jpeg"
+  },
+  {
+    "revision": "e67eec4979609fbe7aab8b979792cdaa",
+    "url": "/img/3.jpeg"
+  },
+  {
+    "revision": "f8cdcd6ae83a757b4ef8ca0994d39e5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adbeec4f9075dc32fbf3",
+    "url": "/js/app.30649175.js"
+  },
+  {
+    "revision": "0f5d83704e08b6290a04",
+    "url": "/js/chunk-vendors.e4342620.js"
+  },
+  {
+    "revision": "16709316661bd6667dbf7e8428767b97",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1be23e1dcd1ca552a61e4bf8abea7684.js"
+  "/precache-manifest.0bd48204189ff68cd5df0742a333d718.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "drivebox"});
